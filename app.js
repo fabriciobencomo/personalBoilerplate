675ba543f35b9519e415e843cd72cc117bc1b480
Fabricio <fabricio@leadboxhq.com>
2024-10-19 18:29:30 -0400

    pug added and create empty pages

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 8080
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index', {
+    meta: {
+        title: 'personal',
+        description: 'Metada description.'
+    }
+  })
 })
 
-app.listen(port,() => {
-    console.log(`app listening at http://localhost:${port}`)
+app.get('/about', (req, res) => {
+    res.render('pages/about')
 })
+
+app.get('/detail:id', (req, res) => {
+    res.render('pages/detail')
+})
+
+app.get('/collection', (req, res) => {
+    res.render('pages/detail')
+})
+
+app.listen(port)
+
 
 
