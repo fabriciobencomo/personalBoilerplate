d017de3ea5122510f76c3473de49a2f09359ba8e
Fabricio <fabricio@leadboxhq.com>
2024-10-20 14:12:56 -0400

    pages added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'pug')
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.render('index', {
+  res.render('pages/home', {
     meta: {
         title: 'personal',
         description: 'Metada description.'
@@ -24,8 +24,8 @@ app.get('/detail:id', (req, res) => {
     res.render('pages/detail')
 })
 
-app.get('/collection', (req, res) => {
-    res.render('pages/detail')
+app.get('/collections', (req, res) => {
+    res.render('pages/collections')
 })
 
 app.listen(port)
