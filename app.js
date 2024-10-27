ad0f5846b4c9ec4c2b64c0d6208153bf141bb708
Fabricio <fabricio@leadboxhq.com>
2024-10-26 20:37:32 -0400

    added styles for floema and many utils classes for boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 
+const logger = require('morgan')
 const express = require('express')
+const errorHandler = require('express-error-handler')
+const bodyParser = require('body-parser') 
+const methodOverride = require('method-override')
+
 const app = express()
 const path = require('path')
 const port = 8080
@@ -19,10 +24,17 @@ const initApi = req => {
 }
 
 const handleLinkResolver = doc => {
-  // if (doc.type === 'blog_post') {
-  //   const date = new Date(doc.first_publication_date)
-  //   return `/${date.getFullYear()}/${doc.uid}`
-  // }
+  if(doc.type == 'product'){
+    return `/detail/${doc.slug}`
+  }
+
+  if(doc.type == 'collections'){
+    return '/collections'
+  }
+
+  if(doc.type == 'about'){
+    return `/about`
+  }
   return '/'
 }
 
@@ -32,45 +44,103 @@ app.use((req, res, next) => {
     linkResolver: handleLinkResolver,
   }
 
+  res.locals.Link = handleLinkResolver
+
   res.locals.PrismicDOM = PrismicDOM
+  res.locals.Numbers = index=>{
+    return index == 0 ? 'One' : index==1 ? 'Two' : index == 2 ? 'Three': index == 3 ? 'Four' : ''
+  }
 
   next()
 })
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride())
+app.use(errorHandler())
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+const handleRequest = async client => {
+  const preloader = await client.getSingle('preloader')
+  const navigation = await client.getSingle('navigation')
+    // Consulta separada para 'meta'
+    const metaResponse = await client.getByType('meta');
+
+  return {
+    meta: metaResponse.results[0],
+    navigation,
+    preloader,
+  }
+}
+
 app.get('/', async (req, res) => {
+  const client = initApi(req);
+  // Consulta separada para 'home'
+  const home = await client.getSingle('home');
+
+  // Consulta separada para 'collection'
+  const {results: collections} = await client.getByType('collection',{
+    fetchLinks: 'product.image'
+  });
+
+  const defaults = await handleRequest(client)
+
   res.render('pages/home', {
-    meta: {
-        title: 'personal',
-        description: 'Metada description.'
-    }
-  })
+    home,
+    collections,
+    ...defaults
+  });
 })
 
 app.get('/about', async (req, res) => {
     const client = initApi(req);
     // Consulta separada para 'about'
     const aboutResponse = await client.getByType('about');
-    console.log('About response:', aboutResponse.results[0].data.body);
 
-    // Consulta separada para 'meta'
-    const metaResponse = await client.getByType('meta');
-    console.log('Meta response:', metaResponse.results[0]);
+    const defaults = await handleRequest(client)
 
     res.render('pages/about', {
-      meta: metaResponse.results[0],
+      ...defaults,
       about: aboutResponse.results[0],
     });
 });
 
-app.get('/detail:id', (req, res) => {
-    res.render('pages/detail')
+app.get('/detail/:uid', async (req, res) => {
+  const client = initApi(req);
+  // Consulta separada para 'product' con su UID 
+  const product = await client.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  });
+
+  const defaults = await handleRequest(client)
+
+  res.render('pages/detail', {
+    ...defaults,
+    product,
+
+  });
 })
 
-app.get('/collections', (req, res) => {
-    res.render('pages/collections')
+app.get('/collections', async (req, res) => {
+  const client = initApi(req);
+  // Consulta separada para 'collection'
+  const {results: collections} = await client.getByType('collection',{
+    fetchLinks: 'product.image'
+  });
+
+   const defaults = await handleRequest(client)
+    // Consulta separada para 'home'
+  const home = await client.getSingle('home');
+
+  res.render('pages/collections', {
+    ...defaults,
+    collections,
+    home,
+  });
 })
 
 app.listen(port)
